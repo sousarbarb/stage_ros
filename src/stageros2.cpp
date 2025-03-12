56a548049c946458d3521c51a9c164f78c91ae31
Ricardo B. Sousa <sousa.ricardob@outlook.com>
2025-03-12 18:57:13 +0000

    Configure ROS 2 CMakeLists + HelloWorld cpp to test libraries linking

---
+++ src/stageros2.cpp
@@ -0,0 +1,51 @@
+/*
+ *  stageros
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  >>>>> Adaptation by Ricardo B. Sousa to make stage_ros compatible with ROS 2
+ */
+
+/**
+
+@mainpage
+
+@htmlinclude manifest.html
+**/
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// libstage
+#include <stage.hh>
+
+// rclcpp
+#include <rclcpp/rclcpp.hpp>
+
+// boost
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/thread.hpp>
+
+int main(int argc, char** argv)
+{
+  std::cout << "Hello Brave New World!" << std::endl;
+  return 0;
+}
